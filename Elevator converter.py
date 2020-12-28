068ab0c65967f8123506dcd900c0bc18f1bcfca2
nathanwilmot <72523140+nathanwilmot@users.noreply.github.com>
2020-12-28 14:51:58 +0000

    Update Elevator converter.py

--- Elevator converter.py
+++ Elevator converter.py
@@ -1,5 +1,6 @@
 #We want to create a program that converts the level of elevater from US to Euro
-
+#euf = EU floor
+#usf = US floor
 usf = input("What floor are you on?")
 euf = int(usf) - 1
 print("You are on floor",euf)
